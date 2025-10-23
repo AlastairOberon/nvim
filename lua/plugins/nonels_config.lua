c5bbb6a474eb79e5a9702109614df02ae8f271ed
AlastairOberon <alastair.gba@gmail.com>
2025-10-23 15:47:21 +0530

    -

--- lua/plugins/nonels_config.lua
+++ lua/plugins/nonels_config.lua
@@ -1,16 +1,20 @@
 return {
 	"nvimtools/none-ls.nvim",
+	--dependencies = { "nvimtools/none-ls-extras.nvim" },
 
 	config = function()
 		local null_ls = require("null-ls")
+		--local extras = require("none-ls-extras")
 
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				-- null_ls.builtins.diagnostics.ruff,
+				--`null_ls.diagnostics.ruff,
+				null_ls.builtins.diagnostics.mypy,
 				null_ls.builtins.formatting.black,
 			},
 		})
 		vim.keymap.set("n", "<M-l>", vim.lsp.buf.format, {})
+		vim.keymap.set("n", "<M-w>", vim.diagnostic.open_float, {})
 	end,
 }
